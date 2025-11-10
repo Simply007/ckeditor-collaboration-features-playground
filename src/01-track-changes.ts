ffd61702bf7f213b1ea21d37a929095aca64c02d
Ondrej Chrastina <9218736+Simply007@users.noreply.github.com>
2025-11-10 17:58:16 +0100

    style and set proper HTML structure

--- src/01-track-changes.ts
+++ src/01-track-changes.ts
@@ -1,9 +1,9 @@
-import { ClassicEditor, Essentials, Mention, Paragraph, Plugin } from 'ckeditor5';
-import { Comments } from 'ckeditor5-premium-features';
+import { ClassicEditor, Essentials, Paragraph, Plugin } from 'ckeditor5';
+import { Comments, TrackChanges } from 'ckeditor5-premium-features';
 import 'ckeditor5/ckeditor5.css';
 import 'ckeditor5-premium-features/ckeditor5-premium-features.css';
 
-// based on https://ckeditor.com/docs/ckeditor5/latest/features/collaboration/comments/comments-integration.html
+// based on https://ckeditor.com/docs/ckeditor5/latest/features/collaboration/track-changes/track-changes-integration.html
 
 class UsersIntegration extends Plugin {
   static get requires() {
@@ -34,18 +34,11 @@ class CommentsIntegration extends Plugin { }
 class TrackChangesIntegration extends Plugin { }
 
 ClassicEditor
-  .create(document.querySelector('#editor3') as HTMLElement, {
+  .create(document.querySelector('#editor') as HTMLElement, {
     licenseKey: import.meta.env.VITE_CKEDITOR_LICENSE_KEY || 'GPL',
-    plugins: [Essentials, Paragraph, Comments, UsersIntegration, Mention],
-    sidebar: { container: document.querySelector('#sidebar3') as HTMLElement },
-    toolbar: ['undo', 'redo', 'comment', 'commentsArchive'],
-    mention: {
-      feeds: [
-        {
-          marker: '@',
-          feed: ['@Barney', '@Lily', '@Marry Ann', '@Marshall', '@Robin', '@Ted'],
-          minimumCharacters: 0
-        }
-      ]
-    }
+    plugins: [Essentials, Paragraph, TrackChanges, Comments, UsersIntegration],
+    toolbar: ['undo', 'redo', 'trackChanges'],
+    sidebar: {
+      container: document.querySelector('#editor-annotations') as HTMLElement
+    },
   })
--- src/03-comments.ts
+++ src/03-comments.ts
@@ -1,9 +1,9 @@
-import { ClassicEditor, Essentials, Paragraph, Plugin } from 'ckeditor5';
-import { Comments, TrackChanges } from 'ckeditor5-premium-features';
+import { ClassicEditor, Essentials, Mention, Paragraph, Plugin } from 'ckeditor5';
+import { Comments } from 'ckeditor5-premium-features';
 import 'ckeditor5/ckeditor5.css';
 import 'ckeditor5-premium-features/ckeditor5-premium-features.css';
 
-// based on https://ckeditor.com/docs/ckeditor5/latest/features/collaboration/track-changes/track-changes-integration.html
+// based on https://ckeditor.com/docs/ckeditor5/latest/features/collaboration/comments/comments-integration.html
 
 class UsersIntegration extends Plugin {
   static get requires() {
@@ -24,18 +24,28 @@ class UsersIntegration extends Plugin {
  *
  * To read more about it, visit the CKEditor 5 documentation: https://ckeditor.com/docs/ckeditor5/latest/features/collaboration/comments/comments-integration.html.
  */
-class CommentsIntegration extends Plugin {}
+class CommentsIntegration extends Plugin { }
 
 /**
  * The `TrackChangesIntegration` lets you synchronize suggestions added to the document with your data source (e.g. a database).
  *
  * To read more about it, visit the CKEditor 5 documentation: https://ckeditor.com/docs/ckeditor5/latest/features/collaboration/track-changes/track-changes-integration.html.
  */
-class TrackChangesIntegration extends Plugin {}
+class TrackChangesIntegration extends Plugin { }
 
 ClassicEditor
-  .create(document.querySelector('#editor') as HTMLElement, {
+  .create(document.querySelector('#editor3') as HTMLElement, {
     licenseKey: import.meta.env.VITE_CKEDITOR_LICENSE_KEY || 'GPL',
-    plugins: [Essentials, Paragraph, TrackChanges, Comments, UsersIntegration],
-    toolbar: ['undo', 'redo', 'trackChanges'],
+    plugins: [Essentials, Paragraph, Comments, UsersIntegration, Mention],
+    sidebar: { container: document.querySelector('#editor-annotations3') as HTMLElement },
+    toolbar: ['undo', 'redo', 'comment', 'commentsArchive'],
+    mention: {
+      feeds: [
+        {
+          marker: '@',
+          feed: ['@Barney', '@Lily', '@Marry Ann', '@Marshall', '@Robin', '@Ted'],
+          minimumCharacters: 0
+        }
+      ]
+    }
   })
